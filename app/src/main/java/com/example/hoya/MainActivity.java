d9a004b00ead6f8b49988d4a0189ddd0dc6cf56f
Menna13 <mkhaliel@brynmawr.edu>
2020-02-01 18:38:35 -0500

    take

--- app/src/main/java/com/example/hoya/MainActivity.java
+++ app/src/main/java/com/example/hoya/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.hoya;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        System.out.println("hahahahahahahahahahahahahahahahah");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void initiateMap(View view){
+        Intent intent = new Intent(this, MapsActivity.class);
+//        EditText editText = (EditText) findViewById(R.id.editText);
+//        String message = editText.getText().toString();
+        String message = "Success";
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/hoya/MapsActivity.java
+++ app/src/main/java/com/example/hoya/MapsActivity.java
@@ -2,20 +2,145 @@ package com.example.hoya;
 
 import androidx.fragment.app.FragmentActivity;
 
+import android.location.Location;
+import android.location.LocationListener;
 import android.os.Bundle;
+import android.widget.Toast;
 
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
-    private GoogleMap mMap;
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+
+/*
+public class MainActivity extends FragmentActivity implements LocationListener {
+
+    GoogleMap map;
 
     @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        GooglePlayServicesUtil
+                .isGooglePlayServicesAvailable(getApplicationContext());
+        map = ((SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map)).getMap();
+        map.setMyLocationEnabled(true);
+        map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        map.addMarker(new MarkerOptions()
+                .position(new LatLng(0, 0))
+                .title("Marker")
+                .draggable(true)
+                .snippet("Hello")
+                .icon(BitmapDescriptorFactory
+                        .defaultMarker(BitmapDescriptorFactory.HUE_YELLOW)));
+
+        map.setOnMarkerDragListener(new OnMarkerDragListener() {
+
+            @Override
+            public void onMarkerDragStart(Marker marker) {
+                // TODO Auto-generated method stub
+                // Here your code
+                Toast.makeText(MainActivity.this, "Dragging Start",
+                        Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onMarkerDragEnd(Marker marker) {
+                // TODO Auto-generated method stub
+                Toast.makeText(
+                        MainActivity.this,
+                        "Lat " + map.getMyLocation().getLatitude() + " "
+                                + "Long " + map.getMyLocation().getLongitude(),
+                        Toast.LENGTH_LONG).show();
+                System.out.println("yalla b2a "
+                        + map.getMyLocation().getLatitude());
+            }
+
+            @Override
+            public void onMarkerDrag(Marker marker) {
+                // TODO Auto-generated method stub
+                // Toast.makeText(MainActivity.this, "Dragging",
+                // Toast.LENGTH_SHORT).show();
+                System.out.println("Draagging");
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    public boolean onMarkerClick(final Marker marker) {
+
+        if (marker.equals(map)) {
+            // handle click here
+            // map.getMyLocation();
+            System.out.println("Clicked");
+            double lat = map.getMyLocation().getLatitude();
+            System.out.println("Lat" + lat);
+            Toast.makeText(MainActivity.this,
+                    "Current location " + map.getMyLocation().getLatitude(),
+                    Toast.LENGTH_SHORT).show();
+        }
+        return true;
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
+ */
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, LocationListener {
+
+    private GoogleMap mMap;
+    String response;
+    String parsedDistance;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
@@ -23,6 +148,51 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(this);
+//        GooglePlayServicesUtil
+//                .isGooglePlayServicesAvailable(getApplicationContext());
+//        mMap = ((SupportMapFragment) getSupportFragmentManager()
+//                .findFragmentById(R.id.map)).getMapAsync(this);
+        mMap.setMyLocationEnabled(true);
+        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        mMap.addMarker(new MarkerOptions()
+                .position(new LatLng(0, 0))
+                .title("Marker")
+                .draggable(true)
+                .snippet("Hello")
+                .icon(BitmapDescriptorFactory
+                        .defaultMarker(BitmapDescriptorFactory.HUE_YELLOW)));
+        mMap.setOnMarkerDragListener(new GoogleMap.OnMarkerDragListener() {
+
+            @Override
+            public void onMarkerDragStart(Marker marker) {
+                // TODO Auto-generated method stub
+                // Here your code
+                Toast.makeText(MainActivity.this, "Dragging Start",
+                        Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onMarkerDragEnd(Marker marker) {
+                // TODO Auto-generated method stub
+                Toast.makeText(
+                        MainActivity.this,
+                        "Lat " + mMap.getMyLocation().getLatitude() + " "
+                                + "Long " + mMap.getMyLocation().getLongitude(),
+                        Toast.LENGTH_LONG).show();
+                System.out.println("yalla b2a "
+                        + mMap.getMyLocation().getLatitude());
+            }
+
+            @Override
+            public void onMarkerDrag(Marker marker) {
+                // TODO Auto-generated method stub
+                // Toast.makeText(MainActivity.this, "Dragging",
+                // Toast.LENGTH_SHORT).show();
+                System.out.println("Draagging");
+            }
+        });
+
     }
 
 
@@ -40,8 +210,124 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng georgtown = new LatLng(39, -77);
+        mMap.addMarker(new MarkerOptions().position(georgtown).title("Marker in Georgetown University"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(georgtown));
+    }
+
+    public String getDistance(final double lat1, final double lon1, final double lat2, final double lon2) {
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    URL url = new URL("http://maps.googleapis.com/maps/api/directions/json?origin=" + lat1 + "," + lon1 + "&destination=" + lat2 + "," + lon2 + "&sensor=false&units=metric&mode=driving");
+                    final HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                    conn.setRequestMethod("POST");
+                    InputStream in = new BufferedInputStream(conn.getInputStream());
+                    response = org.apache.commons.io.IOUtils.toString(in, "UTF-8");
+                    JSONObject jsonObject = new JSONObject(response);
+                    JSONArray array = jsonObject.getJSONArray("routes");
+                    JSONObject routes = array.getJSONObject(0);
+                    JSONArray legs = routes.getJSONArray("legs");
+                    JSONObject steps = legs.getJSONObject(0);
+                    JSONObject distance = steps.getJSONObject("distance");
+                    parsedDistance = distance.getString("text");
+                } catch (ProtocolException e) {
+                    e.printStackTrace();
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        thread.start();
+
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        return parsedDistance;
+    }
+    @Override
+    public void onLocationChanged(Location location) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        // TODO Auto-generated method stub
+
     }
 }
+
+
+//    private double getDistanceInfo(double lat1, double lng1, String destinationAddress) {
+//        StringBuilder stringBuilder = new StringBuilder();
+//        Double dist = 0.0;
+//        try {
+//
+//            destinationAddress = destinationAddress.replaceAll(" ","%20");
+//            String url = "http://maps.googleapis.com/maps/api/directions/json?origin=" + latFrom + "," + lngFrom + "&destination=" + latTo + "," + lngTo + "&mode=driving&sensor=false";
+//
+//            HttpPost httppost = new HttpPost(url);
+//
+//            HttpClient client = new DefaultHttpClient();
+//            HttpResponse response;
+//            stringBuilder = new StringBuilder();
+//
+//
+//            response = client.execute(httppost);
+//            HttpEntity entity = response.getEntity();
+//            InputStream stream = entity.getContent();
+//            int b;
+//            while ((b = stream.read()) != -1) {
+//                stringBuilder.append((char) b);
+//            }
+//        } catch (ClientProtocolException e) {
+//        } catch (IOException e) {
+//        }
+//
+//        JSONObject jsonObject = new JSONObject();
+//        try {
+//
+//            jsonObject = new JSONObject(stringBuilder.toString());
+//
+//            JSONArray array = jsonObject.getJSONArray("routes");
+//
+//            JSONObject routes = array.getJSONObject(0);
+//
+//            JSONArray legs = routes.getJSONArray("legs");
+//
+//            JSONObject steps = legs.getJSONObject(0);
+//
+//            JSONObject distance = steps.getJSONObject("distance");
+//
+//            Log.i("Distance", distance.toString());
+//            dist = Double.parseDouble(distance.getString("text").replaceAll("[^\\.0123456789]","") );
+//
+//        } catch (JSONException e) {
+//            // TODO Auto-generated catch block
+//            e.printStackTrace();
+//        }
+//
+//        return dist;
+//    }
